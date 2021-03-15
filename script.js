1ff4e612fb702977162eab6d0dfd8787fb11345f
Oyvind Sorlie <40733351+heiastart@users.noreply.github.com>
2021-03-15 17:10:46 +0100

    Added another animal to animals object

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const animals = [
   {"id":{"$oid":"60461f6efc13ae2ea6000004"},"animal":"Dove, laughing","scientific name":"Streptopelia senegalensis","amount":3,"location":"Seattle"},
   {"id":{"$oid":"60461f6efc13ae2ea6000005"},"animal":"Malagasy ground boa","scientific name":"Acrantophis madagascariensis","amount":12,"location":"Yako"},
   {"id":{"$oid":"60461f6efc13ae2ea6000006"},"animal":"Dunnart, fat-tailed","scientific name":"Smithopsis crassicaudata","amount":23,"location":"Arvidsjaur"},
-  {"id":{"$oid":"60461f6efc13ae2ea6000007"},"animal":"Otter, african clawless","scientific name":"Aonyx capensis","amount":71,"location":"Zhize"}
+  {"id":{"$oid":"60461f6efc13ae2ea6000007"},"animal":"Otter, african clawless","scientific name":"Aonyx capensis","amount":71,"location":"Zhize"},
+  {"id":{"$oid":"60461f6efc13ae2ea6000008"},"animal":"Reindeer, South-Norway","scientific name":"Reindeer norway","amount":71,"location":"Norway"}
 ]
 
 const animalElements = animals.map(animal => {
